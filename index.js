d3c730a2b76e19a1c78d90b61d23ec936fcac4de
Nyrell Leonor <38891511+nyrellcl@users.noreply.github.com>
2022-03-21 22:28:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'styles.css';
 
+function FadeinSection(props){
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  
+  return (<div className={`fade-in-section ${isVisible ? 'is-Visible' : ''} `}
+  ref={domRef}>
+  {props.children}</div>
+  );
+  }
